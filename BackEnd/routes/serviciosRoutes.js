550f917c5a010609d64118e620cdc1408ccd249b
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 02:39:00 -0300

    agregue servicios.json(13) y cree y conecte ruta servicios/:id

--- BackEnd/routes/serviciosRoutes.js
+++ BackEnd/routes/serviciosRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const ServiciosRouter = express.Router();
 
 // Rutas para servicios
+
 ServiciosRouter.get("/", (req, res) => {
     res.send("Servicios disponibles");   // modificar, pasar logica a los controladores
 });
 
-ServiciosRouter.get("/servicio/:id", (req, res) => {   // modificar, pasar logica a los controladores
+ServiciosRouter.get("/:id", (req, res) => {   // modificar, pasar logica a los controladores
     const servicioId = req.params.id;
     res.send(`Detalles del servicio con ID: ${servicioId}`);
 });
