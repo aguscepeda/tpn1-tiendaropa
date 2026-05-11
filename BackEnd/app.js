b38003ef6d5bf803da0369057d508e6bcdb7c2c9
Santiago <137900941+SantiagoLares@users.noreply.github.com>
2026-05-11 10:53:27 -0300

    Agregue vista de perfil y corregi endpoint perfil

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const serviciosRoutes = require("./routes/serviciosRoutes"); //traigo desde serviciosRoutes las rutas de servicios
 const equipoRoutes = require("./routes/equipoRoutes"); //traigo desde equipoRoutes las rutas de equipo
 const perfilesRoutes = require("./routes/perfilesRoutes"); //traigo desde perfilesRoutes las rutas de perfiles
 
+app.use(cors());
 app.use(express.json()); // Middleware para parsear JSON en las solicitudes entrantes
 
 app.get("/", (req, res) => {
@@ -15,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/servicios", serviciosRoutes);   //cuando se accede a /servicios, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios
 app.use("/equipo", equipoRoutes);   //cuando se accede a /equipo, se redirige a equipoRoutes para manejar las rutas relacionadas con el equipo
-app.use("/perfiles", perfilesRoutes);   //cuando se accede a /perfiles/:id, se redirige a perfilesRoutes para manejar las rutas relacionadas con los perfiles por id
+app.use("/perfil", perfilesRoutes);   //cuando se accede a /perfil/:id, se redirige a perfilesRoutes para manejar las rutas relacionadas con los perfiles por id
 
 module.exports = app;
 
