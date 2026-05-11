8d5954eb692cd4fd31c187f66df82bcc9c1eef27
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 20:33:14 -0300

    agregue models/equipo.js

---
+++ BackEnd/models/equipo.js
@@ -0,0 +1,15 @@
+class Equipo {
+
+    constructor(id, nombre, rol, descripcion, foto) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.rol = rol;
+        this.descripcion = descripcion;
+        this.foto = foto;
+
+    }
+
+}
+
+module.exports = Equipo;
