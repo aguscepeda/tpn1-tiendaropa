e5428c272a908d35cc7f562a7a7e0c23bd0593fe
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 02:30:46 -0300

    hice rutas servicios y las conecte a app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+const serviciosRoutes = require("./routes/serviciosRoutes"); //traigo desde serviciosRoutes las rutas de servicios
+app.use("/servicios", serviciosRoutes);   //cuando se accede a /servicios, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios
+
+app.use("/servicios/:id", serviciosRoutes);   //cuando se accede a /servicios/:id, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios por id
+
 app.get("/", (req, res) => {
   res.send("Servidor funcionando");  //Cuando arranca el servidor, si no esta esto muestra: Cannot GET /
 });
 
 module.exports = app;
+
