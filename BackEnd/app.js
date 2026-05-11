097571461c4e6f51b5090732cb8213de07375f49
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 03:16:42 -0300

    conecte a app.js las rutas de equipoRoutes.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const serviciosRoutes = require("./routes/serviciosRoutes"); //traigo desde serviciosRoutes las rutas de servicios
+const equipoRoutes = require("./routes/equipoRoutes"); //traigo desde equipoRoutes las rutas de equipo
+
+app.use(express.json()); // Middleware para parsear JSON en las solicitudes entrantes
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando");  //Cuando arranca el servidor, si no esta esto muestra: Cannot GET /
@@ -12,7 +15,9 @@ app.get("/", (req, res) => {
 app.use("/servicios", serviciosRoutes);   //cuando se accede a /servicios, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios
 app.use("/servicios/:id", serviciosRoutes);   //cuando se accede a /servicios/:id, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios por id
 
+//RUTAS EQUIPO ------------------------------------------------------------------------------------------------------>
 
+app.use("/equipo", equipoRoutes);   //cuando se accede a /equipo, se redirige a equipoRoutes para manejar las rutas relacionadas con el equipo
 
 
 
