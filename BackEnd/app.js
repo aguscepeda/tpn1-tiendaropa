e93ff25af349690b524a016e38bbe81b329113de
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 03:24:00 -0300

    conecte rutas perfiles a app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const serviciosRoutes = require("./routes/serviciosRoutes"); //traigo desde serviciosRoutes las rutas de servicios
 const equipoRoutes = require("./routes/equipoRoutes"); //traigo desde equipoRoutes las rutas de equipo
+const perfilesRoutes = require("./routes/perfilesRoutes"); //traigo desde perfilesRoutes las rutas de perfiles
 
 app.use(express.json()); // Middleware para parsear JSON en las solicitudes entrantes
 
@@ -19,7 +20,9 @@ app.use("/servicios/:id", serviciosRoutes);   //cuando se accede a /servicios/:i
 
 app.use("/equipo", equipoRoutes);   //cuando se accede a /equipo, se redirige a equipoRoutes para manejar las rutas relacionadas con el equipo
 
+//RUTAS PERFILES ------------------------------------------------------------------------------------------------------>
 
+app.use("/perfiles/:id", perfilesRoutes);   //cuando se accede a /perfiles/:id, se redirige a perfilesRoutes para manejar las rutas relacionadas con los perfiles por id
 
 module.exports = app;
 
