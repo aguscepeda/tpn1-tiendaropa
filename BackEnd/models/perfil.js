47fbb9f7680681d26955f2153c9733bdea70e502
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 20:33:51 -0300

    agregue models/perfil.js

---
+++ BackEnd/models/perfil.js
@@ -0,0 +1,16 @@
+class Perfil {
+
+    constructor(id, nombre, mail, fechaRegistro, foto, ultimosPedidos) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.mail = mail;
+        this.fechaRegistro = fechaRegistro;
+        this.foto = foto;
+        this.ultimosPedidos = ultimosPedidos;
+
+    }
+
+}
+
+module.exports = Perfil;
