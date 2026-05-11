3c1c569a6b4c910c6b9b92eb22fa1fcd78e78be9
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 20:34:21 -0300

    agregue models/usuario.js

---
+++ BackEnd/models/usuario.js
@@ -0,0 +1,15 @@
+class Usuario {
+
+    constructor(id, nombre, mail, password, perfilId) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.mail = mail;
+        this.password = password;
+        this.perfilId = perfilId;
+
+    }
+
+}
+
+module.exports = Usuario;
