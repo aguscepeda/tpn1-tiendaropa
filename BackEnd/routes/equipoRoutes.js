25a43bf4fd84bf5d1b3eadf7722d8b5e52988843
aguscepeda <agustomascepe20@gmail.com>
2026-05-11 03:33:50 -0300

    converti funciones en asincronicas equipoRoutes.js

--- BackEnd/routes/equipoRoutes.js
+++ BackEnd/routes/equipoRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const EquipoRouter = express.Router();
-const equipoData = require('../data/equipo.json'); // Importar datos del equipo desde el archivo JSON
+const fs = require('fs').promises;
+const path = require('path');
 
 // DEVUELVE TODOS LOS MIEMBROS DEL EQUIPO
-EquipoRouter.get('/', (req, res) => {
+EquipoRouter.get('/', async (req, res) => {
     try {
+        const data = await fs.readFile(
+            path.join(__dirname, "../data/equipo.json"),
+            "utf-8"
+        );
+        const equipoData = JSON.parse(data);
         res.json(equipoData); // Enviar la lista completa del equipo como respuesta en formato JSON
     } catch (error) {
         console.error('Error al obtener el equipo en EquipoRouter.get("/"): ', error); // error en el js, datos que no existen, etc
