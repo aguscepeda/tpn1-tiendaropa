30e8ef85db53729352441b431245c91d026726e4
Santiago <137900941+SantiagoLares@users.noreply.github.com>
2026-05-11 11:42:28 -0300

    Agregue login y probe conexion de equipo con api

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
-const serviciosRoutes = require("./routes/serviciosRoutes"); //traigo desde serviciosRoutes las rutas de servicios
-const equipoRoutes = require("./routes/equipoRoutes"); //traigo desde equipoRoutes las rutas de equipo
-const perfilesRoutes = require("./routes/perfilesRoutes"); //traigo desde perfilesRoutes las rutas de perfiles
-
-app.use(cors());
-app.use(express.json()); // Middleware para parsear JSON en las solicitudes entrantes
+const serviciosRoutes = require("./routes/serviciosRoutes"); // traigo desde serviciosRoutes las rutas de servicios
+const equipoRoutes = require("./routes/equipoRoutes"); // traigo desde equipoRoutes las rutas de equipo
+const perfilesRoutes = require("./routes/perfilesRoutes"); // traigo desde perfilesRoutes las rutas de perfiles
+const loginRoutes = require("./routes/loginRoutes"); // traigo desde loginRoutes las rutas de login
+app.use(cors()); // permite que el FrontEnd pueda consumir la API desde otro puerto, por ejemplo Live Server 5500
+app.use(express.json()); // middleware para parsear JSON en las solicitudes entrantes
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando, PAGINA PRINCIPAL");  //Cuando arranca el servidor, si no esta esto muestra: Cannot GET /
+  res.send("Servidor funcionando, PAGINA PRINCIPAL"); // cuando arranca el servidor, si no está esto muestra: Cannot GET /
 });
 
-
-
-app.use("/servicios", serviciosRoutes);   //cuando se accede a /servicios, se redirige a serviciosRoutes para manejar las rutas relacionadas con servicios
-app.use("/equipo", equipoRoutes);   //cuando se accede a /equipo, se redirige a equipoRoutes para manejar las rutas relacionadas con el equipo
-app.use("/perfil", perfilesRoutes);   //cuando se accede a /perfil/:id, se redirige a perfilesRoutes para manejar las rutas relacionadas con los perfiles por id
+app.use("/servicios", serviciosRoutes); // cuando se accede a /servicios, se redirige a serviciosRoutes
+app.use("/equipo", equipoRoutes); // cuando se accede a /equipo, se redirige a equipoRoutes
+app.use("/perfil", perfilesRoutes); // cuando se accede a /perfil/:id, se redirige a perfilesRoutes
+app.use("/login", loginRoutes); // cuando se accede a /login, se redirige a loginRoutes
 
 module.exports = app;
-
